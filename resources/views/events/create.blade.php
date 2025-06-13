e22d4389df4e66aafae6e1f75e90745c97d9614b
Brener Oliveira Brito <=>
2025-06-13 14:01:32 -0300

    Adiciona classe 'space' para espaçamento em formulários de criação de eventos

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -7,22 +7,22 @@
 <div id="event-create-container" class="col-md-6 offset-md-3">
     <h1>Crie o seu evento</h1>
     <form action="/events" method="POST">
-        <div class="form-group">
+        <div class="form-group space">
             <label for="title">Evento:</label>
             <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
         </div>
-        <div class="form-group">
+        <div class="form-group space">
             <label for="title">Cidade:</label>
             <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
         </div>
-        <div class="form-group">
+        <div class="form-group space">
             <label for="title">O evento é privado?</label>
             <select name="private" id="private" class="form-control">
                 <option value="0">Não</option>
                 <option value="1">Sim</option>
             </select>
         </div>
-        <div class="form-group">
+        <div class="form-group space">
             <label for="title">Descrição:</label>
             <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
         </div>
